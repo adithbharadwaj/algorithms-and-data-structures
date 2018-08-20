ce01eaeb90cbb1e0896127446d8d233cd5e14bd8
adith <adithbharadwaj@gmail.com>
2018-08-20 13:34:56 +0530

    iterative tree traversals

--- data_structures_c/iterative_tree_traversals.cpp
+++ data_structures_c/iterative_tree_traversals.cpp
@@ -13,7 +13,17 @@ struct Node
     }
 };
  
-// Iterative function for inorder tree traversal 
+/*
+Algorithm: 
+1) Create an empty stack S.
+2) Initialize current node as root
+3) Push the current node to S and set current = current->left until current is NULL
+4) If current is NULL and stack is not empty then 
+     a) Pop the top item from stack.
+     b) Print the popped item, set current = popped_item->right 
+     c) Go to step 3.
+5) If current is NULL and stack is empty then we are done.
+*/
 void inOrder(struct Node *root)
 {
     stack<Node *> s;
@@ -41,7 +51,18 @@ void inOrder(struct Node *root)
     }
 }
 
-// An iterative process to print preorder traversal of Binary tree
+
+/*
+Algorithm:
+
+1) Create an empty stack nodeStack and push root node to stack.
+2) Do following while nodeStack is not empty.
+….a) Pop an item from stack and print it.
+….b) Push right child of popped item to stack
+….c) Push left child of popped item to stack
+
+Right child is pushed before left child to make sure that left subtree is processed first.
+*/
 void iterativePreorder(struct Node *root)
 {
     // Base Case
@@ -71,6 +92,56 @@ void iterativePreorder(struct Node *root)
             nodeStack.push(node->left);
     }
 }
+
+/*
+Algorithm for iterative postOrder traversal:
+
+We use 2 stacks.
+
+1. Push root to first stack.
+2. Loop while first stack is not empty
+   2.1 Pop a node from first stack and push it to second stack
+   2.2 Push left and right children of the popped node to first stack
+3. Print contents of second stack
+
+*/
+void postOrderIterative(struct Node* root)
+{
+    if (root == NULL) 
+        return;
+ 
+    // Create two stacks
+    stack<Node *> s1;
+    stack<Node *> s2;
+ 
+    // push root to first stack
+    s1.push(root);
+    struct Node* node;
+ 
+    // Run while first stack is not empty
+    while (!s1.empty())
+    {
+        // Pop an item from s1 and push it to s2
+        node = s1.top();
+        s1.pop();
+
+        s2.push(node);
+ 
+        // Push left and right children of removed item to s1
+        if (node->left)
+            s1.push(node->left);
+        if (node->right)
+            s1.push(node->right);
+    }
+ 
+    // Print all elements of second stack
+    while (!s2.empty())
+    {
+        node = s2.top();
+        s2.pop();
+        printf("%d ", node->data);
+    }
+}
  
 
 int main()
@@ -92,6 +163,8 @@ int main()
     inOrder(root);
     cout << endl;
     iterativePreorder(root);
+    cout << endl;
+    postOrderIterative(root);
     cout << endl;
     return 0;
 }
